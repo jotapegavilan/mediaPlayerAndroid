99e0e2d889e25cb86344972dce69f7bbaceba219
Jotape <j.p.daniel.gavilan@gmail.com>
2020-10-30 19:52:22 -0300

    extracción de metadata

--- app/src/main/java/com/gavilan/mediaplayer/MainActivity.java
+++ app/src/main/java/com/gavilan/mediaplayer/MainActivity.java
@@ -5,19 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static android.media.MediaPlayer.TrackInfo.MEDIA_TRACK_TYPE_AUDIO;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
     MediaPlayer mediaPlayer;
     boolean conectado;
     ProgressBar progressBar;
+    IcyStreamMeta streamMeta;
+    MetadataTask2 metadataTask2;
+    String title_artist;
+    String url = "http://stm3.miradio.com.es:12036/"; // your URL here
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +40,23 @@ public class MainActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.progressBar);
         progressBar.setIndeterminate(true);
         conectado = conectar();
+        streamMeta = new IcyStreamMeta();
+
+        try {
+            streamMeta.setStreamUrl(new URL(url));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        metadataTask2 =new MetadataTask2();
+        try {
+            metadataTask2.execute(new URL(url));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Timer timer = new Timer();
+        MyTimerTask task = new MyTimerTask();
+        timer.schedule(task,100, 10000);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +65,8 @@ public class MainActivity extends AppCompatActivity {
                     if (button.getText().equals("PLAY")) {
                         button.setText("PAUSA");
                         mediaPlayer.start();
+                        MediaPlayer.TrackInfo info[] = mediaPlayer.getTrackInfo();
+
                     } else {
                         mediaPlayer.pause();
                         button.setText("PLAY");
@@ -55,12 +86,12 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(),"Intentado conectar",Toast.LENGTH_LONG).show();
 
 
-        String url = "http://stm3.miradio.com.es:12036/"; // your URL here
+
         mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         try {
             mediaPlayer.setDataSource(url);
-            mediaPlayer.prepareAsync();// might take long! (for buffering, etc)
+            mediaPlayer.prepareAsync();
             mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
                 @Override
                 public void onPrepared(MediaPlayer mediaPlayer) {
@@ -75,5 +106,59 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
         return true;
+    }
+
+    protected class MetadataTask2 extends AsyncTask<URL, Void, IcyStreamMeta>
+    {
+        @Override
+        protected IcyStreamMeta doInBackground(URL... urls)
+        {
+            try
+            {
+                streamMeta.refreshMeta();
+                Log.e("Retrieving MetaData","Refreshed Metadata");
+            }
+            catch (IOException e)
+            {
+                Log.e(MetadataTask2.class.toString(), e.getMessage());
+            }
+            return streamMeta;
+        }
+
+        @Override
+        protected void onPostExecute(IcyStreamMeta result)
+        {
+            try
+            {
+                title_artist=streamMeta.getStreamTitle();
+                Log.e("Retrieved title_artist", title_artist);
+                if(title_artist.length()>0)
+                {
+                    Toast.makeText(MainActivity.this,title_artist,Toast.LENGTH_LONG).show();
+                }
+            }
+            catch (IOException e)
+            {
+                Log.e(MetadataTask2.class.toString(), e.getMessage());
+            }
+        }
+    }
+
+    class MyTimerTask extends TimerTask {
+        public void run() {
+            try {
+                streamMeta.refreshMeta();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                String title_artist=streamMeta.getStreamTitle();
+                Log.i("ARTIST TITLE", title_artist);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
     }
 }
